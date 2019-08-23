f132955559d9b9ba97a80ed41feed7840b664811
ZMM <zhang163mingmin@163.com>
2019-08-23 17:25:14 +0800

    modified: 配置修改: 在main.js中集中移入组件

--- src/main.js
+++ src/main.js
@@ -6,12 +6,14 @@ import ElementUI from 'element-ui'
 import 'vue-g2'
 import 'element-ui/lib/theme-chalk/index.css'
 import echarts from 'echarts'
+import common from './components/index'
 
 Vue.prototype.$echarts = echarts
 require('echarts-wordcloud')
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
+Vue.use(common)
 new Vue({
   router,
   store,
